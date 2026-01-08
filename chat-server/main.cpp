7cf4f843d335b94b19a502ae73639c1e7231aad2
Akash K N <akashkncse@gmail.com>
2026-01-08 11:52:54 +0530

    Truncate previous chat server and create new solution folder for better organization and initialize new chat server

--- chat-server/main.cpp
+++ chat-server/main.cpp
@@ -8,55 +8,20 @@
 #include <iostream>
 #pragma comment(lib, "Ws2_32.lib")
 
-
 int main()
 {
-	// Winsock stuff
 	WSADATA wsaData;
-	WORD wVersionReq = MAKEWORD(2, 2);
+	WORD wVersionRequested = MAKEWORD(2, 2);
 	int wsaerr;
-	wsaerr = WSAStartup(wVersionReq, &wsaData);
-	if (wsaerr != 0) {
-		std::cout << "Failed to launch server! " << WSAGetLastError() << std::endl;
+	wsaerr = WSAStartup(wVersionRequested, &wsaData);
+	if (wsaerr != 0)
+	{
+		std::cout << "Failed! Winsock DLL not found!" << std::endl;
 	}
 	else {
 		std::cout << "Success!" << std::endl;
 		std::cout << "The status: " << wsaData.szSystemStatus << std::endl;
 	}
 
-	// Creating the actual socket that will listen
-	SOCKET serverSocket = INVALID_SOCKET;
-	serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (serverSocket == INVALID_SOCKET) {
-		std::cout << "Error at socket(): " << WSAGetLastError() << std::endl;
-		return -1;
-	}
-	else {
-		std::cout << "Success! serversocket is OK!" << std::endl;
-	}
-
-	sockaddr_in service;
-	service.sin_family = AF_INET;
-	u_short port = 8000;
-	service.sin_addr.s_addr = htonl(INADDR_ANY);
-	service.sin_port = htons(port);
-	if (bind(serverSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
-		std::cout << "bind() error: " << WSAGetLastError() << std::endl;
-		closesocket(serverSocket);
-		WSACleanup();
-	}
-	else {
-		std::cout << "bind() is OK!" << std::endl;
-	}
-	if (listen(serverSocket, 1) == SOCKET_ERROR) {
-		std::cout << "listen(): Error listening on socket " << WSAGetLastError() << std::endl;
-	}
-	else {
-		std::cout << "listen() OK!, waiting for connections..." << std::endl;
-	}
-
-	while (true) {
-		
-	}
 
 }
