633685e3ef1da014f545607aefc62fb6b529f140
Akash K N <akashkncse@gmail.com>
2026-01-23 18:25:06 +0530

    feat(posix-sockets): posix server on simple-client-server bind and listen

--- posix-sockets/simple-client-server/server.cpp
+++ posix-sockets/simple-client-server/server.cpp
@@ -3,13 +3,32 @@
 #include <unistd.h>
 #include <cstring>
 #include <iostream>
+#include <arpa/inet.h>
 
-int main() {
+int main()
+{
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd < 0) {
+	if (server_fd < 0)
+	{
 		perror("socket");
 		return 1;
 	}
-
 	sockaddr_in addr{};
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(8000);
+
+	if (bind(server_fd, (sockaddr *)&addr, sizeof(addr)) < 0)
+	{
+		perror("bind error");
+		return 1;
+	}
+
+	if (listen(server_fd, 10) < 0)
+	{
+		perror("listen error");
+		return 1;
+	}
+
+	std::cout << "server listening on 8000";
 }
