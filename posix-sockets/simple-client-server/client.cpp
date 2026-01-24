b09e03162eb69c2177c61264d59ae20031fd9653
Akash K N <akashkncse@gmail.com>
2026-01-24 21:35:59 +0530

    feat(posix-sockets): add simple message transfer between client and server

---
+++ posix-sockets/simple-client-server/client.cpp
@@ -0,0 +1,37 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+#include <iostream>
+#include <arpa/inet.h>
+
+int main()
+{
+    int client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (client_socket == -1)
+    {
+        perror("socket error");
+        return 0;
+    }
+    else
+    {
+        std::cout << "socket() OK!" << std::endl;
+    }
+    sockaddr_in sockinfo;
+    sockinfo.sin_addr.s_addr = inet_addr("127.0.0.1");
+    sockinfo.sin_family = AF_INET;
+    sockinfo.sin_port = htons(8000);
+
+    if (connect(client_socket, (sockaddr *)&sockinfo, sizeof(sockinfo)) == -1)
+    {
+        perror("connect() error");
+        return 0;
+    }
+    else
+    {
+        std::cout << "connect() OK!" << std::endl;
+    }
+    char msg[40] = "hello!";
+    send(client_socket, &msg, 40, 0);
+    return 0;
+}
--- posix-sockets/simple-client-server/server.cpp
+++ posix-sockets/simple-client-server/server.cpp
@@ -7,28 +7,54 @@
 
 int main()
 {
-	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd < 0)
+	int server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (server_socket == -1)
 	{
-		perror("socket");
-		return 1;
+		perror("socket error");
+		return 0;
 	}
-	sockaddr_in addr{};
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(8000);
-
-	if (bind(server_fd, (sockaddr *)&addr, sizeof(addr)) < 0)
+	else
+	{
+		std::cout << "socket() OK!" << std::endl;
+	}
+	sockaddr_in sockinfo;
+	sockinfo.sin_addr.s_addr = INADDR_ANY;
+	sockinfo.sin_family = AF_INET;
+	sockinfo.sin_port = htons(8000);
+	if (bind(server_socket, (sockaddr *)&sockinfo, sizeof(sockinfo)) == -1)
 	{
 		perror("bind error");
-		return 1;
+		return 0;
 	}
-
-	if (listen(server_fd, 10) < 0)
+	else
+	{
+		std::cout << "bind() OK!" << std::endl;
+	}
+	if (listen(server_socket, 10) == -1)
 	{
 		perror("listen error");
-		return 1;
+		return 0;
 	}
+	else
+	{
+		std::cout << "listen() OK!" << std::endl;
+	}
+	int client_socket = accept(server_socket, NULL, NULL);
+	if (client_socket == -1)
+	{
+		perror("accept error");
+		return 0;
+	}
+	else
+	{
+		std::cout << "accept() OK!" << std::endl;
+	}
+	char msg[40];
+	while (recv(client_socket, &msg, 40, 0) != 0)
+		;
+	std::cout << msg << std::endl;
 
-	std::cout << "server listening on 8000";
+	close(client_socket);
+	close(server_socket);
+	return 0;
 }
